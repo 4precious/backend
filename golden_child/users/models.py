972118a7981e725c65a50a26e747988ab50a834e
Subin <supiao0123@gmail.com>
2022-11-13 15:57:24 +0900

    FEAT: edit createsuperuser settings

--- golden_child/users/models.py
+++ golden_child/users/models.py
@@ -1,7 +1,31 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -11,9 +35,10 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     linked_user = models.EmailField(max_length=254)
     created_at = models.DateTimeField(auto_now_add=True)
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
