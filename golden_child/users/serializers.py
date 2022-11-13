4f89d28d28115b9527643b6db176aec52c3f3736
Subin <supiao0123@gmail.com>
2022-11-13 14:24:09 +0900

    FEAT: edit User Model with viewset

--- golden_child/users/serializers.py
+++ golden_child/users/serializers.py
@@ -1,4 +1,3 @@
-from django.core import serializers
 from rest_framework import serializers
 from .models import User
 
@@ -6,9 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'is_parent', 'is_child', 'linked_user']
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=100)
-    is_parent = serializers.BooleanField()
-    is_child = serializers.BooleanField()
+        fields = ('id', 'email', 'password', 'linked_user',
+                  'is_parent', 'is_child', 'created_at')
+
     linked_user = serializers.EmailField(allow_blank=True)
--- golden_child/users/views.py
+++ golden_child/users/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
-
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http.response import HttpResponse
+from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
 
 
-@api_view(['GET'])
-def get_api(request):
-    users = User.objects.all()
-    serailized_users = UserSerializer(users, many=True)
-    return Response(serailized_users.data)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-@api_view(['POST'])
-def post_api(request):
-    if request.method == 'GET':
-        return HttpResponse(status=200)
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if (serializer.is_valid()):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET'])
+# def get_api(request):
+#     users = User.objects.all()
+#     serailized_users = UserSerializer(users, many=True)
+#     return Response(serailized_users.data)
+
+
+# @api_view(['POST'])
+# def post_api(request):
+#     if request.method == 'GET':
+#         return HttpResponse(status=200)
+#     if request.method == 'POST':
+#         serializer = UserSerializer(data=request.data)
+#         if (serializer.is_valid()):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
