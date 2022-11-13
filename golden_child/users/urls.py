5b06b5ef0e81ed5b398a665043c3ddeedde74194
Subin <supiao0123@gmail.com>
2022-11-13 14:24:09 +0900

    FEAT: edit User Model with viewset

--- golden_child/users/urls.py
+++ golden_child/users/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+
 app_name = 'users'
+
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('get/', views.get_api),
-    path('create/', views.post_api),
+    # path('get/', views.get_api),
+    # path('create/', views.post_api),
+    path('', include(router.urls))
 ]
