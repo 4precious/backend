8b3b3edc4912a79fac47e89c784c066156192723
khj777 <hyunjikim1218@gmail.com>
2022-11-18 00:14:42 +0900

    FIX: User permission with user type

--- golden_child/users/serializers.py
+++ golden_child/users/serializers.py
@@ -1,21 +1,34 @@
 from rest_framework import serializers
 from .models import User
+from rest_auth.registration.serializers import RegisterSerializer
 
 
-class UserSerializer(serializers.ModelSerializer):
-    # def create(self, validated_data):
-    #     user = User.objects.create_user(
-    #         email=validated_data['email'],
-    #         password=validated_data['password'],
-    #         linked_user=validated_data['linked_user'],
-    #         is_parent=validated_data['is_parent'],
-    #         is_child=validated_data['is_child']
-    #     )
-    #     return user
 
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'password', 'linked_user',
-                  'is_parent', 'is_child', 'created_at')
+# class UserSerializer(serializers.ModelSerializer):
+#     # def create(self, validated_data):
+#     #     user = User.objects.create_user(
+#     #         email=validated_data['email'],
+#     #         password=validated_data['password'],
+#     #         linked_user=validated_data['linked_user'],
+#     #         is_parent=validated_data['is_parent'],
+#     #         is_child=validated_data['is_child']
+#     #     )
+#     #     return user
 
-    linked_user = serializers.EmailField(allow_blank=True)
+#     class Meta:
+#         model = User
+#         fields = ('id', 'email', 'password', 'linked_user',
+#                   'is_parent', 'is_child', 'created_at')
+
+#     linked_user = serializers.EmailField(allow_blank=True)
+
+class CustomRegisterSerializer(RegisterSerializer):
+    is_child = serializers.BooleanField(default=False)
+    is_parent = serializers.BooleanField(default=False)
+
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['is_child'] = self.validated_data.get('is_child', '')
+        data['is_parent'] = self.validated_data.get('is_parent', '')
+
+        return data
--- golden_child/users/urls.py
+++ golden_child/users/urls.py
@@ -1,13 +1,13 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
+# from django.urls import include, path
+# from rest_framework import routers
+# from . import views
 
-router = routers.DefaultRouter()
-router.register('', views.UserViewSet)
+# router = routers.DefaultRouter()
+# router.register('', views.UserViewSet)
 
-app_name = 'users'
+# app_name = 'users'
 
-urlpatterns = [
-    path('', include(router.urls))
+# urlpatterns = [
+#     path('', include(router.urls))
 
-]
+# ]
