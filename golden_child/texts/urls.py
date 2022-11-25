63d71e709d5cd40785ffd1ef00286afc5ac05dae
Subin <supiao0123@gmail.com>
2022-11-25 16:39:01 +0900

    FEAT: get one answer

--- golden_child/texts/urls.py
+++ golden_child/texts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from texts.views import QuestionViewSet, AnswerViewSet, GetOneQuestionView
+from texts.views import QuestionViewSet, AnswerViewSet, GetOneQuestionView, GetOneAnswerView
 
 router = routers.DefaultRouter()
 router.register(r'question', QuestionViewSet)
@@ -10,5 +10,6 @@ app_name = 'texts'
 
 urlpatterns = [
     path('list/', include(router.urls)),
-    path('single-question/', GetOneQuestionView.as_view(), name='GetQuestion')
+    path('single/question/', GetOneQuestionView.as_view(), name='GetQuestion'),
+    path('single/answer/', GetOneAnswerView.as_view(), name='GetAnswer')
 ]
