0e81da9a70ec95d0adc423aebb0582f24ff8489c
Subin <supiao0123@gmail.com>
2022-11-29 16:22:25 +0900

    FEAT: change path

--- golden_child/manage.py
+++ golden_child/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from ai.inference import BERTDataset, BERTClassifier
 
 
