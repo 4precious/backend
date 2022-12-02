bb5546570d28701d54f5766adddb6895e6b093be
Subin <supiao0123@gmail.com>
2022-12-03 02:38:32 +0900

    FEAT: Add rest authtoken

--- golden_child/golden_child/settings.py
+++ golden_child/golden_child/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'rest_framework',
-    # 'rest_framework.authtoken',
+    'rest_framework.authtoken',
     'rest_auth',
     'allauth',
     'allauth.account',
